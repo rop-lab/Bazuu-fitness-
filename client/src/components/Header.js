ed82fd06cb868b1fabb8c186ddf5662536f94441
Dan-Muchiri <danspmunene@gmail.com>
2024-04-17 21:57:24 +0300

    Drop down for user actions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,3 @@
-// Header.js
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom'; 
 import useAuthStore from './authStore';
@@ -8,10 +7,12 @@ function Header() {
   const { isLoggedIn, username, userPicture, userId, logout, deleteAccount } = useAuthStore();
   const history = useHistory();
   const [isConfirmationOpen, setIsConfirmationOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleLogout = async () => {
     try {
       logout(history);
+      setIsDropdownOpen(false); // Close the dropdown
     } catch (error) {
       console.error('Error logging out:', error);
     }
@@ -25,12 +26,17 @@ function Header() {
     try {
       deleteAccount(userId, () => {
         alert('Your account has been successfully deleted. You are welcome back anytime!');
-        handleLogout();
+        setIsDropdownOpen(false); // Close the dropdown
         setIsConfirmationOpen(false); // Close the confirmation dialog
+        logout(history);
       });
     } catch (error) {
       console.error('Error deleting account:', error);
     }
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   return (
@@ -44,24 +50,27 @@ function Header() {
         </h1>
       </div>
       <div className="head-right-section">
-        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/" className="home-button">Home</Link>
         {isLoggedIn ? (
-          <>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <span style={{ fontSize: '20px', marginRight: '10px' }}>
-                <img src={userPicture} alt="User" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '10px' }} />
-              </span>
-              <span style={{ fontSize: '20px' }}>
-                {username}!
-              </span>
-              <button onClick={handleLogout} className="logout-button">Logout</button>
-              <button onClick={handleDeleteAccount} className="logout-button">Delete Account</button>
-            </div>
-          </>
+          <div style={{ position: 'relative' }}>
+            <span 
+              className="username home-button" 
+              onClick={toggleDropdown}
+            >
+              <img src={userPicture} alt="User" style={{ width: '40px', height: '40px', borderRadius: '50%', marginRight: '10px' }} />
+              {username}
+            </span>
+            {isDropdownOpen && (
+              <div className="dropdown-menu" style={{ position: 'absolute', top: '100%', backgroundColor: '#333', padding: '10px', borderRadius: '5px', zIndex: 999 }}>
+                <button onClick={handleLogout} className="dropdown-button" >Logout</button>
+                <button onClick={handleDeleteAccount} className="dropdown-button ">Delete Account</button>
+              </div>
+            )}
+          </div>
         ) : (
           <>
-            <Link to="/login" className="nav-link">Login</Link>
-            <Link to="/sign-up" className="nav-link">Sign Up</Link>
+            <Link to="/login" className="home-button">Login</Link>
+            <Link to="/sign-up" className="home-button">Sign Up</Link>
           </>
         )}
       </div>
